ea79e21e3e7101fb9c6cce9994216f61e38b6d63
Wilma456 <wilma456@gmx.de>
2017-01-26 20:57:04 +0100

    Create clipboard.lua

---
+++ API/clipboard.lua
@@ -0,0 +1,52 @@
+--Made by Wilma456
+
+function set(con)
+os.clipcon = con
+end
+
+function get()
+return os.clipcon
+end
+
+function getType()
+if os.clipcon then
+  return clipcon.type
+else
+  return "none"
+end
+end
+
+function setText(text)
+os.clipcon = {}
+os.clipcon.type = "text"
+os.clipcon.con = text
+end
+
+function getText()
+if os.clipcon then
+  if os.clipcon.type == "text" then
+    return os.clipcon.con
+  else
+    return ""
+  end
+else
+  return ""
+end
+end
+
+function getTextLine()
+if os.clipcon then
+  if os.clipcon.type == "text" then
+    local linecon = os.clipcon.con:match("([^\n]+)\n([^\n]+)")
+    if linecon == nil then
+      return os.clipcon.con
+    else
+      return linecon
+    end
+  else
+    return ""
+  end
+else
+  return ""
+end
+end
