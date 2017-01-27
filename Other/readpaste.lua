bccabc2e0ed5b12c46eb25c031727f2ff0b0d86c
Wilma456 <wilma456@gmx.de>
2017-01-27 16:20:12 +0100

    Create readpaste.lua

---
+++ Other/readpaste.lua
@@ -0,0 +1,54 @@
+os.loadAPI("clipboard")
+
+local Args = {...}
+
+if not(clipboard) then
+  print("Clipboard API was not found")
+  return
+end
+
+if not(type(oldpasteread) == "function") then
+  oldpasteread = read
+end
+
+function getRead()
+readstr = oldpasteread(Arg1,Arg2,Arg3,Arg4)
+os.queueEvent("exit_clip")
+end
+
+function getClip()
+while true do
+local ev,me = os.pullEvent()
+if ev == "mouse_click" then
+  if me == 3 then
+    os.queueEvent("paste",clipboard.getTextLine())
+  end
+elseif ev == "exit_clip" then
+  break
+end
+end
+end
+
+function read(a,b,c,d)
+Arg1 = a
+Arg2 = b
+Arg3 = c
+Arg4 = d
+parallel.waitForAll(getRead,getClip)
+Arg1 = nil
+Arg2 = nil
+Arg3 = nil
+Arg4 = nil
+local readcopy = readstr
+rreadstr = nil
+return readcopy
+end
+
+if not(Args[1] == "mute") then
+  term.clear()
+  term.setCursorPos(1,1)
+  print("All Done! You can now click the Mousewhell to paste Text from the Clipboard API to Textfiels who are made with read()")
+end
+
+shell.exit()
+shell.run("shell")
