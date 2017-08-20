095d1c4f8f449d081bfe5c6aee31ab1c056144f4
Wilma456 (Jakob0815) <wilma456@gmx.de>
2017-08-20 19:30:34 +0200

    Update to Version 2.0

--- Programs/textpaint.lua
+++ Programs/textpaint.lua
@@ -43,7 +43,7 @@ end
 
 local tField = {}
 
-local function clear()
+local function clear(sColor)
 for i=1,nScreenh-1 do
     tField[i] = {}
     tField[i]["text"] = ""
@@ -52,21 +52,21 @@ for i=1,nScreenh-1 do
     end
     tField[i]["color"] = ""
     for a=1,nScreenw-1 do
-        tField[i]["color"] = tField[i]["color"].."0"
+        tField[i]["color"] = tField[i]["color"]..sColor
     end
     tField[i]["background"] = ""
     for a=1,nScreenw-1 do
-        tField[i]["background"] = tField[i]["background"].."0"
+        tField[i]["background"] = tField[i]["background"]..sColor
     end
 end
 end
-clear()
+clear("0")
 
 local function replacechar(pos, str, r)
     return str:sub(1, pos-1) .. r .. str:sub(pos+1)
 end
 
--- Create .nfp files by default
+-- Create .nft files by default
 if not fs.exists( sFile ) and not string.find( sFile, "%." ) then
     local sExtension = ".nft"
     if sExtension ~= "" then
@@ -82,23 +82,25 @@ if fs.exists(sFile) == true then
     for i=1,nScreenh-1 do
         local nCou = 1
         local sLine = fileh.readLine()
-        for a = 1, #sLine do
-            local c = sLine:sub(a,a)
-            if c == "\30" then
-                sMode = "background"
-            elseif c == "\31" then
-                sMode = "color"
-            elseif sMode == "background" then
-                nReadBackground = c
-                sMode = "text"
-            elseif sMode == "color" then
-                nReadColor = c
-                sMode = "text"
-            elseif sMode == "text" then
-                tField[i]["text"] = replacechar(nCou,tField[i]["text"],c)
-                tField[i]["background"] = replacechar(nCou,tField[i]["background"],nReadBackground)
-                tField[i]["color"] = replacechar(nCou,tField[i]["color"],nReadColor)
-                nCou = nCou + 1
+        if type(sLine) == "string" then
+            for a = 1, #sLine do
+                local c = sLine:sub(a,a)
+                if c == "\30" then
+                    sMode = "background"
+                elseif c == "\31" then
+                    sMode = "color"
+                elseif sMode == "background" then
+                    nReadBackground = c
+                    sMode = "text"
+                elseif sMode == "color" then
+                    nReadColor = c
+                    sMode = "text"
+                elseif sMode == "text" then
+                    tField[i]["text"] = replacechar(nCou,tField[i]["text"],c)
+                    tField[i]["background"] = replacechar(nCou,tField[i]["background"],nReadBackground)
+                    tField[i]["color"] = replacechar(nCou,tField[i]["color"],nReadColor)
+                    nCou = nCou + 1
+                end
             end
         end
     end
@@ -164,6 +166,8 @@ local function redraw()
             term.write(" ")
         end
     end
+    term.setCursorPos(nScreenw,17)
+    term.blit(" ",nColor,nColor)
     term.setCursorPos(1,nScreenh)
     term.clearLine()
     term.setTextColor(colors.white)
@@ -175,6 +179,23 @@ end
 
 redraw()
 
+local function addChar(sChar)
+    if bWrite == true then
+        tField[nPosY]["text"] = replacechar(nPosX,tField[nPosY]["text"],sChar)
+        tField[nPosY]["color"] = replacechar(nPosX,tField[nPosY]["color"],nColor)
+        nPosX = nPosX + 1
+        if nPosX == nScreenw then
+            if nPosY == nScreenh-1 then
+                bWrite = false
+            else
+                nPosY = nPosY + 1
+                nPosX = 1
+            end
+        end
+        redraw()
+    end
+end
+
 while true do
     local ev,me,x,y = os.pullEvent()
     if ev == "mouse_click" then
@@ -184,6 +205,8 @@ while true do
                     nBackground = tColors[y]
                     nColor = tColors[y]
                     term.setTextColor(tHex[nColor])
+                    term.setCursorPos(nScreenw,17)
+                    term.blit(" ",nColor,nColor)
                 end
             elseif x ~= nScreenw and y ~= nScreenh then
                 tField[y]["background"] = replacechar(x,tField[y]["background"],nBackground)
@@ -217,7 +240,7 @@ while true do
                 elseif x > 5 and x < 10 then
                     break
                 elseif x > 10 and x < 16 then
-                    clear()
+                    clear(nColor)
                     redraw()
                 end
             end
@@ -238,6 +261,8 @@ while true do
         tField[y]["background"] = replacechar(x,tField[y]["background"],nBackground)
         redraw()
     elseif ev == "char" and bWrite == true then
+        addChar(me)
+        --[[
         tField[nPosY]["text"] = replacechar(nPosX,tField[nPosY]["text"],me)
         tField[nPosY]["color"] = replacechar(nPosX,tField[nPosY]["color"],nColor)
         nPosX = nPosX + 1
@@ -250,6 +275,12 @@ while true do
             end
         end
         redraw()
+        ]]--
+    elseif ev == "paste" then
+        for i = 1, #me do
+            local c = me:sub(i,i)
+            addChar(c)
+        end
     elseif ev == "key" and bWrite == true then
         if me == keys.enter then
             if nPosY ~= nScreenh-1 then
@@ -269,6 +300,18 @@ while true do
             tField[nPosY]["text"] = replacechar(nPosX,tField[nPosY]["text"]," ")
             tField[nPosY]["color"] = replacechar(nPosX,tField[nPosY]["color"],nColor)
             redraw()
+        elseif me == keys.up and nPosY ~= 1 then
+            nPosY = nPosY - 1
+            term.setCursorPos(nPosX,nPosY)
+         elseif me == keys.down and nPosY ~= nScreenh-1 then
+            nPosY = nPosY + 1
+            term.setCursorPos(nPosX,nPosY)
+         elseif me == keys.left and nPosX ~= 1 then
+            nPosX = nPosX - 1
+            term.setCursorPos(nPosX,nPosY)
+        elseif me == keys.right and nPosX ~= nScreenw-1 then
+            nPosX = nPosX + 1
+            term.setCursorPos(nPosX,nPosY)
         end
     end
 end
