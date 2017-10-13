96b7824ff206c984d0eb3095fe44a502c4f1bdbd
Wilma456 (Jakob0815) <wilma456@gmx.de>
2017-10-13 19:48:26 +0200

    Update to Version 3.0

--- Programs/virtualos-manager.lua
+++ Programs/virtualos-manager.lua
@@ -5,7 +5,9 @@ local bSelect = false
 local tList = {}
 local nSelect
 local w,h = term.getSize()
-local sVersion = "2.0"
+local sVersion = "3.0"
+local tEmptyMenu = {}
+local tSelectMenu = {}
 
 if fs.exists(fs.combine(fs.getDir(shell.getRunningProgram()),"virtualos.lua")) then
     sVirtualPath = "/"..fs.combine(fs.getDir(shell.getRunningProgram()),"virtualos.lua")
@@ -43,9 +45,13 @@ term.setCursorPos(1,1)
 term.clearLine()
 term.setTextColor(colors.black)
 if bSelect then
-    term.write("Run Delete New Link About")
+    for k,v in ipairs(tSelectMenu) do
+        term.write(v.text.." ")
+    end
 else
-    term.write("New About")
+    for k,v in ipairs(tEmptyMenu) do
+        term.write(v.text.." ")
+    end
 end
 term.setCursorPos(w,1)
 term.blit("X","f","e")
@@ -72,12 +78,45 @@ if os.version():sub(9) == tList[nSelect]["version"] then
 else
     sRomPath = fs.combine(sDir,"rom/"..tList[nSelect]["version"])
 end
-local sExtra = " \"--title=CraftOS "..tList[nSelect]["version"].."\""
+local sExtra = " \"--title="..tList[nSelect]["title"].."\" --id="..tList[nSelect]["id"]
 if tonumber(tList[nSelect]["version"]) <= 1.5 then
     sExtra = sExtra.." --oldnative"
 end
 if tList[nSelect]["version"] == "1.2" then
     sExtra = sExtra.." --diskapi"
+end
+if tList[nSelect]["http"] == false then
+    sExtra = sExtra.." --nohttp"
+end
+if tList[nSelect]["sharefolder"] == true then
+    sExtra = sExtra.." --sharePath="..tList[nSelect]["sharepath"].." --shareName="..tList[nSelect]["sharename"]
+end
+if tList[nSelect]["time"] == false then
+    sExtra = sExtra.." --notime"
+end
+if tList[nSelect]["day"] == false then
+    sExtra = sExtra.." --noday"
+end
+if tList[nSelect]["setlabel"] == true then
+    sExtra = sExtra.." --label="..tList[nSelect]["label"]
+end
+if tList[nSelect]["allowper"] == false then
+    sExtra = sExtra.." --noper"
+end
+if tList[nSelect]["epoch"] == false then
+    sExtra = sExtra.." --noepoch"
+end
+if tList[nSelect]["diskmount"] then
+    sExtra = sExtra.." --diskmount"
+end
+if tList[nSelect]["turtleapi"] == false then
+    sExtra = sExtra.." --noturtle"
+end
+if tList[nSelect]["pocketapi"] == false then
+    sExtra = sExtra.." --nopocket"
+end
+if tList[nSelect]["commandapi"] == false then
+    sExtra = sExtra.." --nocomand"
 end
 return sVirtualPath.." --biosPath="..fs.combine(sDir,"bios/"..tList[nSelect]["version"])..".lua --romPath="..sRomPath.." --rootPath="..fs.combine(sDir,"drive/"..tList[nSelect]["name"])..sExtra
 end
@@ -185,10 +224,27 @@ fs.makeDir(fs.combine(sDir,"drive/"..name))
 local tmpta = {}
 tmpta.name = name
 tmpta.version = ver
+tmpta.http = true
+tmpta.sharefolder = false
+tmpta.sharepath = "/"
+tmpta.sharename = "share"
+tmpta.time = true
+tmpta.day = true
+tmpta.setlabel = false
+tmpta.label = name
+tmpta.id = "0"
+tmpta.title = "CraftOS "..ver
+tmpta.allowper = true
+tmpta.epoch = true
+tmpta.diskmount = false
+tmpta.turtleapi = true
+tmpta.pocketapi = true
+tmpta.commandapi = true
 table.insert(tList,tmpta)
 local file = fs.open(fs.combine(sDir,sSaveFile),"w")
 file.write(textutils.serialize(tList))
 file.close()
+redrawMenu()
 end
 
 local function newLink()
@@ -230,6 +286,134 @@ VirtualOS and VirtualOS-Manager are both licensed under the BSD 2-clause "Simpli
 redrawMenu()
 end
 
+local function run()
+    term.clear()
+    term.setCursorPos(1,1)
+    if shell.run(getCommand()) == false then
+        print("Press any Key to Continue")
+        os.pullEvent("key")
+    end
+    redrawMenu()
+end
+
+local function delete()
+    fs.delete(fs.combine(sDir,"/drive/"..tList[nSelect]["name"]))
+    table.remove(tList,nSelect)
+    bSelect = false
+    local file = fs.open(fs.combine(sDir,sSaveFile),"w")
+    file.write(textutils.serialize(tList))
+    file.close()
+    redrawMenu()
+end
+
+local function writeBoolean(bBool)
+    if bBool == true then
+        term.blit("true","5555","0000")
+    else
+        term.blit("false","eeeee","00000")
+    end
+end
+
+local function edit()
+    local tObj = tList[nSelect]
+    local tOptions = {}
+    tOptions[1] = {"boolean","Enable Http: ","http"}
+    tOptions[2] = {"boolean","Enable Shared Folder: ","sharefolder"}
+    tOptions[3] = {"text","Shared Folder Path: ","sharepath"}
+    tOptions[4] = {"text","Shared Folder Name: ","sharename"}
+    tOptions[5] = {"boolean","Enable os.time(): ","time"}
+    tOptions[6] = {"boolean","Enable os.day(): ","day"}
+    tOptions[7] = {"boolean","Set Computer Label: ","setlabel"}
+    tOptions[8] = {"text","Computer Label: ","label"}
+    tOptions[9] = {"number","Computer ID: ","id"}
+    tOptions[10] = {"text","Multishell Title: ","title"}
+    tOptions[11] = {"boolean","Allow Peripherals: ","allowper"}
+    tOptions[12] = {"boolean","Enable os.epoch(): ","epoch"}
+    tOptions[13] = {"boolean","Give acces to Diskdata: ","diskmount"}
+    tOptions[14] = {"boolean","Enable Turtle API ","turtleapi"}
+    tOptions[15] = {"boolean","Enable Pocket API ","pocketapi"}
+    tOptions[16] = {"boolean","Enable Commands API ","commandapi"}
+    while true do
+        term.setBackgroundColor(colors.white)
+        term.setTextColor(colors.black)
+        term.clear()
+        term.setBackgroundColor(colors.blue)
+        term.setCursorPos(1,h)
+        term.clearLine()
+        term.write("OK")
+        term.setBackgroundColor(colors.white)
+        for k,v in ipairs(tOptions) do
+            term.setCursorPos(1,k)
+            if v[1] == "boolean" then
+                term.write(v[2])
+                writeBoolean(tObj[v[3]])
+            elseif v[1] == "text" or v[1] == "number" then
+                term.write(v[2]..tObj[v[3]])
+            end
+        end
+        local ev,me,x,y = os.pullEvent()
+        if ev == "mouse_click" then
+            if type(tOptions[y]) == "table" then
+                if tOptions[y][1] == "boolean" then
+                    tObj[tOptions[y][3]] = not(tObj[tOptions[y][3]])
+                elseif tOptions[y][1] == "text" then
+                    term.setCursorPos(#tOptions[y][2]+1,y)
+                    tObj[tOptions[y][3]] = read(nil,nil,nil,tObj[tOptions[y][3]])
+                elseif tOptions[y][1] == "number" then
+                    term.setCursorPos(#tOptions[y][2]+1,y)
+                    local tmp = read(nil,nil,nil,tObj[tOptions[y][3]])
+                    if tonumber(tmp) == nil then
+                        showText("You can only insert Numbers here")
+                    else
+                        tObj[tOptions[y][3]] = tmp
+                    end
+                end
+            elseif y == h then
+                break
+            end
+        end
+    end
+    local file = fs.open(fs.combine(sDir,sSaveFile),"w")
+    file.write(textutils.serialize(tList))
+    file.close()
+    redrawMenu()
+end
+
+local function copy()
+    term.clear()
+    term.setCursorPos(1,1)
+    term.setBackgroundColor(colors.blue)
+    term.clearLine()
+    term.write("Please choose a name:")
+    term.setBackgroundColor(colors.white)
+    term.setCursorPos(1,2)
+    local name = read()
+    if name == "" then
+        showText("The Name can't be empty")
+        redrawMenu()
+        return
+    elseif name:find(" ") ~= nil then
+        showText("No spaces allowed")
+        redrawMenu()
+        return
+    end
+    for k,v in ipairs(tList) do
+        if v.name == name then
+            showText("This Name does already exists")
+            redrawMenu()
+            return
+        end
+    end
+    local tmpta = {}
+    for k,v in pairs(tList[nSelect]) do
+        tmpta[k] = v
+    end
+    tmpta.name = name
+    table.insert(tList,tmpta)
+    fs.copy(fs.combine(sDir,"/drive/"..tList[nSelect]["name"]),fs.combine(sDir,"/drive/"..name))
+    redrawMenu()
+end
+
 if fs.exists(fs.combine(sDir,sSaveFile)) then
     local file = fs.open(fs.combine(sDir,sSaveFile),"r")
     tList = textutils.unserialise(file.readAll())
@@ -237,6 +421,46 @@ if fs.exists(fs.combine(sDir,sSaveFile)) then
 else
     tList = {}
 end
+
+local function backwardsBoolean(bRe,bDe)
+    if bRe == nil then
+        return bDe
+    else
+        return bRe
+    end
+end
+
+--Backwards Compatibylity
+for k,v in ipairs(tList) do
+    v.http = backwardsBoolean(v.http,true)
+    v.sharefolder = backwardsBoolean(v.sharefolder,false)
+    v.sharepath = v.sharepath or "/"
+    v.sharename = v.sharename or "share"
+    v.time = backwardsBoolean(v.time,true)
+    v.day = backwardsBoolean(v.day,true)
+    v.setlabel = backwardsBoolean(v.setlabel,false)
+    v.label = v.label or v.name
+    v.id = v.id or "0"
+    v.title = v.title or "CraftOS "..v.version
+    v.allowper = backwardsBoolean(v.allowper,true)
+    v.epoch = backwardsBoolean(v.epoch,true)
+    v.diskmount = backwardsBoolean(v.diskmount,false)
+    v.turtleapi = backwardsBoolean(v.turtleapi,true)
+    v.pocketapi = backwardsBoolean(v.pocketapi,true)
+    v.commandapi = backwardsBoolean(v.commandapi,true)
+end
+
+tEmptyMenu[1] = {text="New",func=newMachine}
+tEmptyMenu[2] = {text="About",func=about}
+
+tSelectMenu[1] = {text="Run",func=run}
+tSelectMenu[2] = {text="Delete",func=delete}
+tSelectMenu[3] = {text="Edit",func=edit}
+tSelectMenu[4] = {text="Copy",func=copy}
+tSelectMenu[5] = {text="New",func=newMachine}
+tSelectMenu[6] = {text="Link",func=newLink}
+tSelectMenu[7] = {text="About",func=about}
+
 redrawMenu()
 
 while true do
@@ -244,39 +468,29 @@ while true do
     if ev == "mouse_click" then
         if y == 1 then
             if bSelect == false then
-                if x < 4 then
-                    newMachine()
-                    redrawMenu()
-                elseif x > 4 and x < 10 then
-                    about()
-                elseif x == w then
+                local nMenupos = 0
+                for key,menuitem in ipairs(tEmptyMenu) do
+                    if x > nMenupos and x < menuitem.text:len()+nMenupos+1 then
+                        menuitem.func()
+                        break 
+                    else
+                    nMenupos = nMenupos+menuitem.text:len()+1
+                    end
+                end
+                if x == w then
                     break
                 end
             elseif bSelect == true then
-                if x < 4 then
-                    term.clear()
-                    term.setCursorPos(1,1)
-                    if shell.run(getCommand()) == false then
-                        print("Press any Key to Continue")
-                        os.pullEvent("key")
+                local nMenupos = 0
+                for key,menuitem in ipairs(tSelectMenu) do
+                    if x > nMenupos and x < menuitem.text:len()+nMenupos+1 then
+                        menuitem.func()
+                        break 
+                    else
+                    nMenupos = nMenupos+menuitem.text:len()+1
                     end
-                    redrawMenu()
-                elseif x > 4 and x < 11 then
-                    fs.delete(fs.combine(sDir,"/drive/"..tList[nSelect]["name"]))
-                    table.remove(tList,nSelect)
-                    bSelect = false
-                    local file = fs.open(fs.combine(sDir,sSaveFile),"w")
-                    file.write(textutils.serialize(tList))
-                    file.close()
-                    redrawMenu()
-                elseif x > 11 and x < 15 then
-                    newMachine()
-                    redrawMenu()
-                elseif x > 15 and x < 20 then
-                    newLink()
-                elseif x > 20 and x < 26 then
-                    about()
-                elseif x == w then
+                end
+                if x == w then
                     break
                 end
             end
